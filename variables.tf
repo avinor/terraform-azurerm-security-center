db9ab37b836ea34af84a4d71cb7d893184fe9d3b
Pavlo Prykhodko <pavlo.prykhodko@avinor.no>
2021-06-24 14:17:57 +0200

    Add missing variables. Fix test issues

--- variables.tf
+++ variables.tf
@@ -1,6 +1,15 @@
-
 variable "resource_types" {
   description = "List of resource types to which Azure Defender should be enabled"
   type = list(string)
   default = []
+}
+
+variable "location" {
+  description = "The Azure Region in which to create resource."
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default     = {}
 }
